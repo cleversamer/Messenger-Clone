31e6916e3d33d39145e30b659f15a8e70badb113
Samer A <thedev.samer@gmail.com>
2022-03-29 08:15:34 +0300

    did the basic steps to build the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import "./css/app.css";
 
 const App = () => {
-  return <div>Hello World!</div>;
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState(["Hey Guys!"]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    setMessages([...messages, input]);
+    setInput("");
+  };
+
+  return (
+    <div className="app">
+      <h1>Hello World!</h1>
+
+      <form>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.currentTarget.value)}
+        />
+
+        <button type="submit" onClick={sendMessage}>
+          Send Message
+        </button>
+      </form>
+
+      {/* messages */}
+    </div>
+  );
 };
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase";
+
+const config = {
+  apiKey: "AIzaSyCYOwDdHhU-ImIEpX5fWV8L8eV9sqz3s1A",
+  authDomain: "samer-messenger.firebaseapp.com",
+  projectId: "samer-messenger",
+  storageBucket: "samer-messenger.appspot.com",
+  messagingSenderId: "343558646562",
+  appId: "1:343558646562:web:aeb2cfd8c256388f67537b",
+  measurementId: "G-6QMEE53CWP",
+};
+
+const app = initializeApp(config);
+
+export const db = app.firestore();
